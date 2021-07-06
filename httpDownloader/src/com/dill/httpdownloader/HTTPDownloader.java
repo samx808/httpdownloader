c977dcc2aee867bfddbf218ddb06769d6efead0a
samx808 <84256955+samx808@users.noreply.github.com>
2021-07-06 14:53:04 +1000

    Cleaned up unused code

--- httpDownloader/src/com/dill/httpdownloader/HTTPDownloader.java
+++ httpDownloader/src/com/dill/httpdownloader/HTTPDownloader.java
@@ -1,5 +1,6 @@
 package com.dill.httpdownloader;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -8,18 +9,17 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.Scanner;
 
-public class HTTPDownloader {
 
-	@SuppressWarnings("resource")
+public class HTTPDownloader {
+	
+	public static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) throws IOException {
 		
 		downloader();
 		
-		Scanner repeatInput = new Scanner(System.in);
 		System.out.print("Would you like to download more files? (Type 'yes' or 'no'): ");
-		String repeat = repeatInput.nextLine();
-//		System.out.println(repeat);
-//		repeatInput.close();
+		String repeat = scanner.nextLine();
 		
 		if (repeat.equals("no")) {
 			System.out.println("Goodbye");
@@ -30,29 +30,26 @@ public class HTTPDownloader {
 
 	}
 	
-	@SuppressWarnings("resource")
 	public static void downloader() throws IOException {
 		
 		try {	
-			Scanner fileURLInput = new Scanner(System.in);
+			
 			System.out.print("URL of file to download: ");
-			String fileURL = fileURLInput.nextLine();
-//			System.out.println(fileURL);
-//			fileURLInput.close();
+			String fileURL = scanner.nextLine();
 			
-			Scanner fileOutputUsrInput = new Scanner(System.in);
 			System.out.print("Name of downloaded file (include extension): ");
-			String filenameOutput = fileOutputUsrInput.nextLine();
-//			fileOutputUsrInput.close();
+			String filenameOutput = scanner.nextLine();
 			
-			URL website = new URL(fileURL);
-			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-			FileOutputStream fos = new FileOutputStream(filenameOutput);
-			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-			fos.close();
+			try {
+				URL website = new URL(fileURL);
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				FileOutputStream fos = new FileOutputStream(filenameOutput);
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("Invalid Filename");
+			}
 			
-//			fileURLInput.close();
-//			fileOutputUsrInput.close();
 			
 		} catch (MalformedURLException e) {
 			System.out.println("Invalid URL");
